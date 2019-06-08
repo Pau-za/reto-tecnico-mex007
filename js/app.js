841780abab6441d775e1af8c28f2d73f4262802c
Pau-za <molo_pau@hotmail.com>
2019-06-07 19:01:15 -0500

    agrega estilo a la página. Listo para ser enviado :D

--- js/app.js
+++ js/app.js
@@ -7,20 +7,22 @@ fetch('https://retos-tecnicos-backend.lizzie136.now.sh/bands')
     }).then(data => {
         // const orderedBands = bands.sort;
         const bands = Object.values(data);
-        console.log(bands);
         bands.forEach(band => {
-            palceToRender1.insertAdjacentHTML('beforeend', `<tr><td>${band}</tr></td>`)
-        })
+            palceToRender1.insertAdjacentHTML('beforeend', `<tr><td>${band}</td></tr>`)
+        });
         const ignoreArts = (bandName) => {
             return bandName.replace(/^(an?|the)\s/i, '');
-        }
-        bands.sort((a, b) => {
-            return ignoreArts(a).localeCompare(ignoreArts(b));
+        };
+
+        document.getElementById('button-order').addEventListener('click', () => {
+            bands.sort((a, b) => {
+                return ignoreArts(a).localeCompare(ignoreArts(b));
+            })
+            bands.forEach(element => {
+                return document.getElementById('band-name').insertAdjacentHTML('beforeend', `<tr><td>${element}</td></tr>`)
+            });
+            document.getElementById('new-ordered').style.display = 'block';
         })
-        bands.forEach(element => {
-            document.getElementById('band-name').insertAdjacentHTML('beforeend', `<tr><td>${element}</tr></td>`)
-        });
-        return bands;
     })
     .catch(function (error) {
         if (error) {
